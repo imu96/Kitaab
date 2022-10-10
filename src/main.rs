3ad5dc158fb94b938dadc3d510b840b8bb7ce240
Imran Saleh <imran96saleh@gmail.com>
2022-10-10 23:57:57 +0500

    Changes UI to resemble interactive git add.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 use std::io;
+use colored::Colorize;
 mod adhoc;
 
 fn main() {
 
     // Welcome message
-    println!("Ya Ali Madad and welcome to the Clifton Library
-    Management Software!\n");
+    println!("Welcome to Kitaab- a library management software!");
 
     // String in which most recent command will be stored
     let mut cmd = String::new();
 
+    // Options of what to do
+    let options = ["lend","return","renew", "pay fines", "search",
+		   "add", "edit", "quit", "help"];
+
     loop {
-	println!("> ");
+	println!("{}", "*** Commands ***".bold());
+
+	for i in 0..options.len() {
+	    // Prints new line if four options are on the line
+	    if i % 4 == 0 {
+		println!("");
+	    }
+	    print!("{}: {.1}{} \t \t",i,
+		   format!(options[i]).bold().blue(),
+		   options[i][1..]);
+	}
+
+	print!("What now>");
 	io::stdin()
 	    .read_line(&mut cmd)
 	    .expect("Failed to read line. Please try again");
 	let cmd_args : Vec<&str> = cmd.split_whitespace().collect();
 	match cmd_args[0] {
+
 	    // lends a book to a member
 	    "lend" => lend(& cmd_args),
 	    // returns a book 
