561face7b7bd6430b5244a24d2b38b015b351c15
Imran Saleh <imran96saleh@gmail.com>
2022-11-09 21:51:48 +0500

    Adds structs to add new rows to tables
    Renames Loans struct to Issues

--- src/models.rs
+++ src/models.rs
@@ -17,6 +17,16 @@ pub struct Book {
     pub isbn: String,
 }
 
+#[derive(Insertable)]
+#[table_name = "books"]
+pub struct NewBook {
+    pub title: String,
+    pub auth_fst: String,
+    pub auth_lst: String,
+    pub curr_status: Status,
+    pub isbn: String,
+}
+
 #[derive(Queryable)]
 pub struct Member {
     pub id: u32,
@@ -25,10 +35,27 @@ pub struct Member {
     pub dob: Date<UTC>,
 }
 
+#[derive(Insertable)]
+#[table_name = "members"]
+pub struct NewMember {
+    pub fst_name: String,
+    pub lst_name: String,
+    pub dob: Date<UTC>,
+}
+
 #[derive(Queryable)]
-pub struct Loan {
+pub struct Issue {
     pub id: u32,
     // doi stands for date of issue
+    pub doi: Date<UTC>,
+    pub due: Date<UTC>,
+    pub bk_id: u32,
+    pub mbr_id: u32,
+}
+
+#[derive(Insertable)]
+#[table_name = "loans"]
+pub struct NewIssue {
     pub doi: Date<UTC>,
     pub due: Date<UTC>,
     pub bk_id: u32,
