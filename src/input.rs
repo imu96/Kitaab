21e7c48121c8481cdaa4dfd272de8b8c53a8e671
Imran Saleh <imran96saleh@gmail.com>
2022-11-14 22:19:42 +0500

    Adds confirm fn to inputs, since confirming input will happen a lot

--- src/input.rs
+++ src/input.rs
@@ -1,5 +1,24 @@
 use std::io::stdin;
 
+pub fn confirm() -> bool {
+    loop {
+	println!("(1) yes\t\t(2) no");
+	let yn= get_opt();
+	if yn > 2 {
+	    println!("Number entered does not correspond to an
+    option. Please enter number in the given range");
+	    continue;
+	}
+	else if yn == 1 {
+	    return true;
+	}
+	else {
+	    return false;
+	}
+    }
+}
+		
+
 // Gets number from user
 pub fn get_num() -> u32 {
     let mut opt = String::new();
